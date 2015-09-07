2edff539e2f68e498ff0b2aa2751bdcd472058e2
yuanqi <yuanqi@cairenhui.com>
2015-09-07 21:19:51 +0800

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
 var nodemon = require('gulp-nodemon');
 
-
-gulp.task('default', ['browser-sync'], function () {
+//dev task start
+gulp.task('sass', function () {
+  return gulp.src('public/scss/*.scss')
+    .pipe(sass({outputStyle: 'compressed', sourceComments: 'map'}, {errLogToConsole: true}))
+    .pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR"))
+    .pipe(gulp.dest('public/css'))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('browser-sync', ['nodemon'], function () {
@@ -16,16 +24,24 @@ gulp.task('browser-sync', ['nodemon'], function () {
     port: 5000
   });
 });
+
 gulp.task('nodemon', function (cb) {
 
-  var started = false;
+  var called = false;
 
   return nodemon({
     script: 'bin/www'
   }).on('start', function () {
-    if (!started) {
+    if (!called) {
       cb();
-      started = true;
+      called = true;
     }
   });
+});
+//dev task end
+
+//build task start
+//TODO add build task
+//build task end
+gulp.task('default', ['browser-sync'], function () {
 });
