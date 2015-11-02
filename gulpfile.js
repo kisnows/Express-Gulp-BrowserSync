e502baf3fc9f4b356ee04908d2020a7f693b94c8
yuanqi <yuanqi@cairenhui.com>
2015-11-02 16:41:25 +0800

    调整gulp任务

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,32 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var nodemon = require('gulp-nodemon');
-var sourcemaps = require('gulp-sourcemaps');
-var jade = require('gulp-jade');
-var del = require('del');
+var reload      = browserSync.reload;
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var nodemon     = require('gulp-nodemon');
+var sourcemaps  = require('gulp-sourcemaps');
+var jade        = require('gulp-jade');
+var del         = require('del');
 
 //dev task start
-//TODO can not compile the sass or less file
+//DONE can not compile the sass or less file
 gulp.task('sass', function () {
-  return gulp.src('./sass/main.scss')
+  return gulp.src(['./sass/main.scss'])
     .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        errLogToConsole: true,
-        outputStyle: 'compressed'
-      }).on('error', sass.logError))
-    .pipe(prefix('last 2 versions', 'ie 8', 'Android 4.0'))
+    .pipe(sass({errLogToConsole: true})
+      .on('error', sass.logError))
+    .pipe(prefix('last 2 versions', 'ie 8', 'Android 4'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./public/css'))
-    .pipe(reload({ stream: true }));
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('browser-sync', ['nodemon'], function () {
-  del(['./public/*.html']);
   browserSync.init(null, {
-    proxy: 'http://localhost:3000',
-    files: ['public/**/*.*', 'views/**/*.*'],
+    proxy: 'http://localhost:3002',
+    files: ['public/**/*.*', 'views/**/*.*', 'submodule/**/*.*'],
     browser: 'google chrome',
     notify: false,
     port: 5000
@@ -38,7 +34,6 @@ gulp.task('browser-sync', ['nodemon'], function () {
 });
 
 gulp.task('nodemon', function (cb) {
-  del(['./public/*.html']);
 
   var called = false;
 
@@ -51,25 +46,43 @@ gulp.task('nodemon', function (cb) {
     }
   });
 });
+
 //dev task end
 
+
 //build task start
-//TODO add build task
+//DONE add build task
 gulp.task('jade', function () {
   return gulp.src([
     'views/**/*.jade',
-    '!views/includes/*.jade',
-    '!views/layout/*.jade',
-    '!views/error.jade'
+    '!views/error.jade',
+    '!views/components/**/*.jade',
+    '!views/includes/**/*.jade',
+    '!views/index-parts/*.jade',
+    '!views/layout/**/*.jade'
   ])
-    .pipe(jade({ pretty: true }))
-    .pipe(gulp.dest('./public/views'));
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest('./dist'));
 });
-
-gulp.task('build', ['jade']);
 //build task end
 
+gulp.task('clean', function (cb) {
+  del(['./dist/*'], cb)
+});
+
+gulp.task('copy', function () {
+  return gulp.src([
+    'public/css/**/*',
+    'public/images/**/*',
+    'public/js/**/*'
+  ], {base: './public'})
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('dist', ['clean', 'copy', 'jade']);
+
+
 gulp.task('default', ['browser-sync', 'sass'], function () {
-  gulp.watch('sass/**/*.*', ['sass']);
+  gulp.watch(['sass/**/*.*'], ['sass']);
 });
 
